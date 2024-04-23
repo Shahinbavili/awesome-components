e990093b648a75c2ea4119866c3cc6b6272dfe54
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-04-23 18:09:39 +0200

    add animations

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -1,11 +1,33 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Comment} from "../../../core/models/comment.model";
 import {FormBuilder, FormControl, Validators} from "@angular/forms";
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
+  animations: [
+    trigger('listItem', [
+      state('default', style({
+        transform: 'scale(1)',
+        backgroundColor: 'white',
+        zIndex: 1
+      })),
+      state('active', style({
+        transform: 'scale(1.05)',
+        'background-color': 'rgb(201, 157, 242)',
+        'z-index': 2,
+        'border-radius': '10px',
+      })),
+      transition('default => active', [
+        animate('100ms ease-in-out')
+      ]),
+      transition('active => default', [
+        animate('500ms ease-in-out')
+      ]),
+    ])
+  ]
 })
 export class CommentsComponent implements OnInit {
   @Input() comments!: Comment[];
@@ -13,12 +35,18 @@ export class CommentsComponent implements OnInit {
 
   commentCtrl!: FormControl;
 
+  listItemAnimationState: ('default' | 'active')[] = [];
+
+
   constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.commentCtrl = this.formBuilder.control('',
       [Validators.required, Validators.minLength(10)]);
+    if (this.comments) {
+      this.listItemAnimationState = new Array(this.comments.length).fill('default')
+    }
   }
 
   onLeaveComment() {
@@ -27,5 +55,13 @@ export class CommentsComponent implements OnInit {
     }
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
+  }
+
+  onListItemMouseEnter(index: number) {
+    this.listItemAnimationState[index] = 'active';
+  }
+
+  onListItemMouseLeave(index: number) {
+    this.listItemAnimationState[index] = 'default';
   }
 }
