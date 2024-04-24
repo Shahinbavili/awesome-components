4e443ec01aaafb688f3bb9da4e4705f69cb2d723
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-04-24 13:44:06 +0200

    animate the new comment

--- src/app/shared/components/comments/comments.component.ts
+++ src/app/shared/components/comments/comments.component.ts
@@ -26,6 +26,18 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
       transition('active => default', [
         animate('500ms ease-in-out')
       ]),
+      transition('void => *', [
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+          'background-color': 'rgb(201, 157, 242)',
+        }),
+        animate('250ms ease-out', style({
+          transform: 'translateX(0)',
+          opacity: 1,
+          backgroundColor: 'white',
+        }))
+      ])
     ])
   ]
 })
@@ -53,6 +65,13 @@ export class CommentsComponent implements OnInit {
     if (this.commentCtrl.invalid) {
       return;
     }
+    const maxId = Math.max(...this.comments.map(comment => comment.id))
+    this.comments.unshift({
+      id: maxId + 1,
+      comment: this.commentCtrl.value,
+      createdDate: new Date().toISOString(),
+      userId: 1
+    })
     this.newComment.emit(this.commentCtrl.value);
     this.commentCtrl.reset();
   }
