5777b5673e605876a8c9aeff533fd4de25233cb5
shahin <shahin.aboulhassanibavili2021@campus-eni.fr>
2024-10-16 06:44:21 +0200

    Use Enum for search type

--- src/app/reactive-state/components/candidate-list/candidate-list.component.ts
+++ src/app/reactive-state/components/candidate-list/candidate-list.component.ts
@@ -1,7 +1,10 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {CandidatesService} from "../../services/candidates.service";
-import {Observable} from "rxjs";
+import {combineLatest, Observable} from "rxjs";
 import {Candidate} from "../../models/candidate.model";
+import {FormBuilder, FormControl} from "@angular/forms";
+import {CandidateSearchType} from "../../enums/candidate-search.type";
+import {map, startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-candidate-list',
@@ -14,17 +17,55 @@ export class CandidateListComponent implements OnInit {
   loading$!: Observable<boolean>;
   candidates$!: Observable<Candidate[]>;
 
-  constructor(private candidatesService: CandidatesService) {
+  searchCtrl!: FormControl;
+  searchTypeCtrl!: FormControl;
+  searchTypeOptions!: {
+    value: CandidateSearchType,
+    label: string
+  }[];
+
+  constructor(private candidatesService: CandidatesService,
+              private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.initForm();
     this.initObservables();
     this.candidatesService.getCandidatesFromServer();
   }
 
+  private initForm() {
+    this.searchCtrl = this.formBuilder.control('');
+    this.searchTypeCtrl = this.formBuilder.control(CandidateSearchType.LASTNAME);
+    this.searchTypeOptions = [
+      {value: CandidateSearchType.LASTNAME, label: 'Nom'},
+      {value: CandidateSearchType.FIRSTNAME, label: 'Prénom'},
+      {value: CandidateSearchType.COMPANY, label: 'Entreprise'}
+    ]
+  }
+
   private initObservables() {
     this.loading$ = this.candidatesService.loading$;
-    this.candidates$ = this.candidatesService.candidates$;
+    const search$ = this.searchCtrl.valueChanges.pipe(
+      startWith(this.searchCtrl.value),
+      map(value => value.toLowerCase())
+    );
+    const searchType$: Observable<CandidateSearchType> = this.searchTypeCtrl.valueChanges.pipe(
+      startWith(this.searchTypeCtrl.value),
+    );
+    this.candidates$ = combineLatest([
+      search$,
+      searchType$,
+      this.candidatesService.candidates$
+    ]).pipe(
+      map(([search, searchType, candidates]) =>
+        candidates.filter(candidate => candidate[searchType]
+          .toLowerCase()
+          .includes(search as string))
+      )
+    );
+
   }
+
 
 }
